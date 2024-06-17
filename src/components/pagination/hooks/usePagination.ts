871e3c3cd7b6611865b11f139c9d4a528bb54c19
lunaticscode <lunatics384@gmail.com>
2024-06-17 18:14:01 +0900

    ui 패키지 업데이트 - Pagination > usePagination 수정

--- src/components/pagination/hooks/usePagination.ts
+++ src/components/pagination/hooks/usePagination.ts
@@ -1,8 +1,11 @@
 import { useMemo, useState } from "react";
 
-// const getMemorizedRule = () => {
-
-// }
+const getMemorizedRule = (currentPage: number, divider: number) => {
+  // 1 ~ 10까지에서는 아무 숫자나 업데이트 되더라도 기존 생성된 Array를 그대로 사용
+  // 단, 11이 됬을 때는 11 ~ 20에 해당되는 새로운 Array를 생성
+  console.log(currentPage, (Math.ceil(currentPage / divider) - 1) * divider);
+  return (Math.ceil(currentPage / divider) - 1) * divider; // 1 ~ 10 => 0, 11 ~ 20 => 10
+};
 const DEFAULT_DIVIDER = 10;
 const usePagination = ({ itemLength = 0, divider = DEFAULT_DIVIDER }) => {
   // 현재 사용자가 바라보고 있는 페이지 번호;
@@ -26,6 +29,7 @@ const usePagination = ({ itemLength = 0, divider = DEFAULT_DIVIDER }) => {
     [currentPage, divider, itemLength]
     // [getMemorizedRule(currentPage, divider)]
   );
+
   const pages: Array<number> = useMemo(
     () =>
       Array.from(
@@ -33,8 +37,8 @@ const usePagination = ({ itemLength = 0, divider = DEFAULT_DIVIDER }) => {
         (_, index) =>
           (Math.ceil(currentPage / divider) - 1) * divider + (index + 1)
       ),
-    [currentPage, divider, pageLength]
-    // [getMemorizedRule(currentPage, divider)]
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [getMemorizedRule(currentPage, divider)]
   );
 
   return { pages, setCurrentPage, currentPage, isLastPage, isFirstPage };
